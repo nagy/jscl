5bc8a87d62d837a1e890fc0d9307b0013547aa65
Daniel Nagy <danielnagy@posteo.de>
2021-09-30 22:55:29 +0200

    Show amount of failed tests, if any

--- tests-report.lisp
+++ tests-report.lisp
@@ -2,8 +2,10 @@
         (/ (- (get-internal-real-time) *timestamp*) internal-time-units-per-second 1.0))
 
 (if (= *passed-tests* *total-tests*)
-    (format t "All the tests (~a) passed successfully.~%" *total-tests*)
-    (format t "~a/~a test(s) passed successfully.~%" *passed-tests* *total-tests*))
+  (format t "All the tests (~a) passed successfully.~%" *total-tests*)
+  (progn
+    (format t "~a/~a test(s) passed successfully.~%" *passed-tests* *total-tests*)
+    (format t "~a test(s) failed.~%" (- *total-tests* *passed-tests*))))
 
 (terpri)
 
